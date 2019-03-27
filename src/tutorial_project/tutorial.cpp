3ea3fdae51c0edb7e06d3c3ce8e3641a367c73f8
SupasanKomonlit <supasan_224@hotmail.com>
2019-03-27 22:51:22 +0700

    Add version detail and macro by CMakeLists.txt?

--- src/tutorial_project/tutorial.cpp
+++ src/tutorial_project/tutorial.cpp
@@ -3,18 +3,22 @@
 #include    <math.h>
 #include    <vector>
 
+#include    "tutorial_project_config.h"
+
 int main( int argc , char *argv[] )
 {
+    printf("data of tutorial_project_VERSION_MAJOR : %d\n" , tutorial_project_VERSION_MAJOR );
+    printf("data of tutorial_project_VERSION_MINOR : %d\n\n" , tutorial_project_VERSION_MINOR );
     std::vector< unsigned char > buffer;
     buffer.reserve( 100 );
-    for( int run = 0 ; run < 10 ; run++ )
+    for( int run = 0 ; run < 15 ; run++ )
     {
         buffer.push_back( run );
     }
     
     printf("size member     : %ld\n" , buffer.size() );
     printf("capacity buffer : %ld\n" , buffer.capacity() );
-    printf("max size buffer : %ld\n" , buffer.max_size() );
+    printf("max size buffer : %d\n" , buffer.max_size() );
 
     return 0;
 }
